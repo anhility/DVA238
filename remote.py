ba9e604b1b37491b2dca27cf40756d4af479eff2
anhility <anhility@gmail.com>
2018-10-16 14:02:03 +0000

    annan fix av sträng

--- remote.py
+++ remote.py
@@ -101,7 +101,7 @@ def threadRequestFile():
         b1 = GPIO.input(PIN_B1)
         b2 = GPIO.input(PIN_B2)
         if b1 == False: # Without flash
-            sendUDP(str(MSG_TAKEPIC) =+ '_t' + time.time())
+            sendUDP(str(MSG_TAKEPIC =+ '_t' + time.time()))
             with open("image.jpg", 'wb') as picFile:
                 conn, IP_TRG = SKT_T.accept()
                 while True:
@@ -114,7 +114,7 @@ def threadRequestFile():
             conn.close()
 
         if b2 == False: # With flash
-            sendUDP(str(MSG_TAKEPICF) =+ '_t' + time.time())
+            sendUDP(str(MSG_TAKEPICF =+ '_t' + time.time()))
             with open("imageF.jpg", 'wb') as picFile:
                 conn, IP_TRG = SKT_T.accept()
                 while True:
