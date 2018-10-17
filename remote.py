a7f1373046ca172d25899ebf0ac63a8068e45619
anhility <anhility@gmail.com>
2018-10-17 11:39:20 +0000

    fix av sträng

--- remote.py
+++ remote.py
@@ -141,7 +141,7 @@ def threadRequestFile():
             t_T = t_end - t_T
             print("UDP+TCP:", t_UT,
                 "\nTCP:    ", t_T)
-            str_L = "\"tcpL\"," , str(t_T) + ",\"udpL+tcpL\"," + str(t_UT) + "\n"
+            str_L = "\"tcpL\"," + str(t_T) + ",\"udpL+tcpL\"," + str(t_UT) + "\n"
             writeLog(str_L)
 
         time.sleep(POLL_TIME / 1000.0)
